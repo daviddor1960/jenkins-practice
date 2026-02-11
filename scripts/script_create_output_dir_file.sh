aa3240a671801adbda7cb4cc11a6ffe5e7ad542e
david <daviddor@tutamail.com>
2026-02-11 11:44:59 +0200

    adding the github author

--- scripts/script_create_output_dir_file.sh
+++ scripts/script_create_output_dir_file.sh
@@ -8,12 +8,20 @@ else
   echo "Directory outputs already exists"
 fi
 
-# 2. Check/Create File
+# 2. Get the last commit author
+# %an is the Author Name, %ae is the Author Email
+COMMIT_AUTHOR=$(git log -1 --format='%an <%ae>')
+
+# 3. Check/Create File
 # Check if running inside Jenkins to grab the build number
 if [ -n "$BUILD_NUMBER" ]; then
   # echo "Jenkins Build Number: #$BUILD_NUMBER"
-  echo "Current Date-Time: $(date), Jenkins Build Number: #$BUILD_NUMBER"  >> "./outputs/my_output_file.txt"
+  #echo "Current Date-Time: $(date), Jenkins Build Number: #$BUILD_NUMBER"  >> "./outputs/my_output_file.txt"
+  echo "Date-Time: $(date), Build: #$BUILD_NUMBER, Author: $COMMIT_AUTHOR" >> "./outputs/my_output_file.txt"
+
 else
   #echo "Jenkins Build Number: Not running in Jenkins"
-  echo "Current Date/Time: $(date)" >> "./outputs/my_output_file.txt"
+  #echo "Current Date/Time: $(date)" >> "./outputs/my_output_file.txt"
+  echo "Date-Time: $(date), Author: $COMMIT_AUTHOR" >> "./outputs/my_output_file.txt"
 fi
+
